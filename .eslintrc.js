348d160e975ad084b7ffb982a131d6f63c9bf4f3
Horizon-git <apbreloaded947@gmail.com>
2024-03-05 16:31:30 +0200

    connecting to the host

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: ['@mate-academy/eslint-config-react-typescript'],
+  rules: {
+    'max-len': ['error', {
+      ignoreTemplateLiterals: true,
+      ignoreComments: true,
+    }],
+    'jsx-a11y/label-has-associated-control': ["error", {
+      assert: "either",
+    }],
+    'no-console': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-noninteractive-element-interactions': 'off',
+    'no-case-declarations': 'off',
+  },
+};
